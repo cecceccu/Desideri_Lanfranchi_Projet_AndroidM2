a993310806aac05c592f90acd5344113f2a5a699
Mickael Lanfranchi <58193062+cecceccu@users.noreply.github.com>
2021-11-09 15:44:56 +0100

    Populated spinner

--- app/src/main/java/com/example/desideri_lanfranchi_projet_androidm2/MainActivity.kt
+++ app/src/main/java/com/example/desideri_lanfranchi_projet_androidm2/MainActivity.kt
@@ -2,11 +2,27 @@ package com.example.desideri_lanfranchi_projet_androidm2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Utils.generateAirportList()
+        val airportList = Utils.generateAirportList()
+        val airportNamesList = ArrayList<String>()
+        for (airport in airportList)
+        {
+            airportNamesList.add(airport.getFormattedName())
+        }
+
+        val adapter: ArrayAdapter<String> = ArrayAdapter<String>(
+            this, android.R.layout.simple_spinner_item,
+            airportNamesList
+        )
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+
+        findViewById<Spinner>(R.id.airport_spinner).adapter = adapter
     }
 }
